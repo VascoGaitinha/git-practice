92097b6982920a04356dd353afd29bd6da803a2c
Vasco Gaitinha <vascomiguel.gaitinha@gmail.com>
2023-10-05 20:08:05 +0100

    update

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 function getData(){
     let userName = document.getElementById("userName").value;
     let userAge = document.getElementById("userAge").value;
-    
-
     alert(`Name: ${userName}\nAge: ${userAge}`)
+}
+
+function checkCity(){
+    let choice = document.getElementsByName("city").value
+    
+    alert(`Name: ${choice}`)
 }
