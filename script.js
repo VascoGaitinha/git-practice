83226b3b1c69a4c89bdfa201e0f3bd75a49cbf2d
Vasco Gaitinha <vascomiguel.gaitinha@gmail.com>
2023-10-05 19:31:26 +0100

    last working

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function getData(){
+    let userName = document.getElementById("userName").value;
+    let userAge = document.getElementById("userAge").value;
+    
+
+    alert(`Name: ${userName}\nAge: ${userAge}`)
+}
