7feb2675d9559edf46520c58ba65e6c331be9534
Vasco Gaitinha <vascomiguel.gaitinha@gmail.com>
2023-10-06 01:51:56 +0100

    6.10.close

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 
 function getData(){
-    
-    let userName = document.getElementById("userName").value;
+    let userName1 = document.getElementById("userName1").value;
+    let userName2 = document.getElementById("userName2").value;
     let userAge = document.getElementById("userAge").value;  
+    let userGender = checkGender()
     let userCity = checkCity()
-    alert(`Name: ${userName}\nAge: ${userAge}\nCity: ${userCity}`)
+    let userSkills = checkSkills()
+    alert(`Please confirm submited data:\n\nName: ${userName1} ${userName2}\nAge: ${userAge}\nGender: ${userGender}\nCity: ${userCity}\nSkills: ${userSkills}`)
 }
 
-function checkCity(){
-    const city = "";
-    document.getElementsByName("city")
+function checkCity(){    
+   let cities
+
+   document.getElementsByName("city")
     .forEach (radio => {
-        if(radio.checked){
-            alert(city)}}
+        if(radio.checked === true){
+            cities=radio.value
+        }
+    }
     )
+    return cities
+}
+
+function checkGender(){
+    let gender ="ERROR"    
+    return gender
+}
+
+function checkSkills(){
+    let gender ="ERROR"    
+    return gender
 }
 
